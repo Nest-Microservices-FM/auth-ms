f43d8d55fbd2e127eacc985274031cc5d415f693
HX-FMino <fmino@soyhenry.com>
2024-05-01 19:37:05 -0300

    password encryption and first connection to the gateway

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,22 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller()
 export class AuthController {
+
   constructor(private readonly authService: AuthService) {}
 
 
   @MessagePattern('auth.register.user')
-  registerUser(){
-    return 'register user'
+  registerUser(@Payload() registeruserDto:RegisterUserDto){
+    return this.authService.registerUser(registeruserDto);
   }
 
   @MessagePattern('auth.login.user')
-  loginUser(){
-    return 'login user';
+  loginUser(@Payload() loginUserDto:LoginUserDto){
+    return this.authService.loginUser(loginUserDto);
   }
 
   @MessagePattern('auth.verify.user')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,95 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import { RegisterUserDto } from './dto/register-user.dto';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
 @Injectable()
-export class AuthService {}
+export class AuthService extends PrismaClient implements OnModuleInit{
+
+  private readonly logger = new Logger('AuthService')
+
+
+  onModuleInit(){
+    this.$connect();
+    this.logger.log('MongoDB connected')
+  }
+
+
+  async registerUser(registerUserDto:RegisterUserDto){
+    try {
+      const {name, email, password} = registerUserDto;
+      const user = await this.user.findUnique({
+        where:{
+          email: email,
+
+        }
+      });
+
+    if( user ){
+      throw new RpcException({ 
+        status:400,
+        message: 'User already exists'
+      })
+    }
+
+    const newUser = await this.user.create({
+      data: {
+        email: email,
+        password: bcrypt.hashSync(password, 10),
+        name: name
+      }
+    })
+
+    const { password: __, ...rest } = newUser
+    return {
+      user: rest, 
+      token: 'ABC'
+    }
+    } catch (error) {
+      throw new RpcException({
+        status:400,
+        message: error.message
+      })
+    }
+  }
+
+  async loginUser(loginUserDto:LoginUserDto){
+    try {
+      const {email, password} = loginUserDto;
+      
+      const user = await this.user.findUnique({
+        where:{
+          email: email,
+
+        }
+      });
+
+      if( !user ){
+        throw new RpcException({ 
+          status:400,
+          message: 'User/Password not valid'
+        })
+      }
+
+      const isPasswordValid = bcrypt.compareSync(password, user.password)
+      if( !isPasswordValid){
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid'
+        })
+      }
+
+      const { password: __, ...rest } = user;
+      return {
+        user: rest, 
+        token: 'ABC'
+      }
+    } catch (error) {
+      throw new RpcException({
+        status:400,
+        message: error.message
+      })
+    }
+  }
+}
